4b61d9813e4841f9c0d6d40bb8c3fa3c5a50a872
guoxiaopei <53297067+guoxiaopei@users.noreply.github.com>
2020-01-09 13:19:32 +0800

    addfavorite and addcart

---
+++ src/components/TopNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col cols="2">
+        <img :src="require('@/assets/logo.png')" alt="">
+      </b-col>
+      <b-col cols="8">
+        <b-form-input id="input-default" placeholder="课程搜索"></b-form-input>
+        <b-button variant="success">
+          <em id="search"></em>
+        </b-button>
+      </b-col>
+      <b-col cols="2"></b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "TopNav"
+
+}
+</script>
+
+<style>
+
+</style>
